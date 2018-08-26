216fb0e8da575eec452a735a91ae1181e5de84c0
Franco Barletta <franco.barletta@dubitlimited.com>
2018-08-26 12:10:41 -0300

    draw bg

--- src/play.js
+++ src/play.js
@@ -1,7 +1,23 @@
+
+
 var play = {
     preload: function () {
     },
     create: function () {
+        //bg
+        const divisions = 6;
+        const colors = [0x1FC8DC,0x1EBCCF]
+        var graphics = this.add.graphics(0, 0);
+        for(var i = 0; i < divisions; i++) {
+            graphics.fillStyle(colors[i%2]);
+            var x = 0;
+            var h = Math.round(SCENE_HEIGHT / divisions);
+            var y = h * i;
+            var w = SCENE_WIDTH;
+            graphics.fillRect(x, y, w, h);
+        }
+        //end bg
+
         console.log("Scene Play");
         var monster = this.add.sprite(SCENE_WIDTH/2, 0, "monster");
         monster.y = monster.height/2;
