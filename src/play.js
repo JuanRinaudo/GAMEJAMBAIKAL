32f70d7be581bed8aaf19b6d563a8a428b3613f8
Juan Rinaudo <juanerinaudo@gmail.com>
2018-08-26 12:39:17 -0300

    Player movement and shooting, some constant added.

--- src/play.js
+++ src/play.js
@@ -1,5 +1,23 @@
 var ship;
-var lazers = [];
+var playerProjectiles;
+var monsterProjectiles;
+
+var playerHealth = 0;
+var monsterHealth = 0;
+
+var spaceBar;
+var left;
+var right;
+
+var cursors;
+
+var shootTimer = 0;
+var SHIP_FIRERATE = 100;
+var SHIP_SPEED = 0.5;
+
+var MONSTER_HEALTH = 100;
+var PLAYER_HEALTH = 5;
+var LAZER_DAMAGE = 1;
 
 var play = {
     preload: function () {
@@ -9,13 +27,30 @@ var play = {
         var monster = this.add.sprite(SCENE_WIDTH/2, 0, "monster");
         monster.y = monster.height/2;
 
-        ship = this.add.image(game.canvas.width * 0.5, game.canvas.height - 64, 'ship');
-    },
-    update: function() {
+        ship = this.add.sprite(game.canvas.width * 0.5, game.canvas.height - 64, 'ship');
         
-        // lazers = this.add.image(400, 150, 'logo');
-        if(true) {
+        playerProjectiles = this.add.group();
+        spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
+        cursors = this.input.keyboard.createCursorKeys();
+    },
+    update: function(time, deltaTime) {
+        if(spaceBar.isDown) {
+            shootTimer += deltaTime;
+            if(shootTimer > SHIP_FIRERATE) {
+                var lazer = this.add.sprite(ship.x, ship.y, 'lazer');
+                playerProjectiles.add(lazer);
+                shootTimer = 0;
+            }
+        }
+        var speed = (left.isDown ? -1 : 0 + right.isDown ? 1 : 0) * deltaTime * SHIP_SPEED;
+        ship.x = Math.min(Math.max(ship.x + speed, 32), SCENE_WIDTH - 32);
+
+        for(var i = 0; i < playerProjectiles.children.size; i++) {
+            var lazer = playerProjectiles.children.entries[i];
+            lazer.y -= deltaTime;
         }
     }
 }
