af9668d8c3c5b90edd85e10c041b835e52e487d7
Juan Rinaudo <juanerinaudo@gmail.com>
2018-08-26 12:01:56 -0300

    Merged and pushed.

--- src/play.js
+++ src/play.js
@@ -1,3 +1,6 @@
+var ship;
+var lazers = [];
+
 var play = {
     preload: function () {
     },
@@ -5,6 +8,15 @@ var play = {
         console.log("Scene Play");
         var monster = this.add.sprite(SCENE_WIDTH/2, 0, "monster");
         monster.y = monster.height/2;
+
+        ship = this.add.image(game.canvas.width * 0.5, game.canvas.height - 64, 'ship');
+    },
+    update: function() {
+        
+        // lazers = this.add.image(400, 150, 'logo');
+        if(true) {
+
+        }
     }
 }
 
