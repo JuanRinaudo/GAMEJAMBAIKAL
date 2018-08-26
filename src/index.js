f18e0f0c6d93ea716d8f83aa0f4e21ce84c39fea
Franco Barletta <franco.barletta@dubitlimited.com>
2018-08-26 13:23:32 -0300

    add keypress to skip

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const SCENE_HEIGHT = 640;
 const TEXT_TIMEOUT = 50;
 const END_TIMEOUT = 2000;
 
+var timerId;
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
@@ -29,7 +30,6 @@ game.scene.start('load');
 
 
 function preload() {
-    this.load.image('logo', 'assets/logo.png');
     this.load.image('monster', 'assets/Monster.png');
     this.load.image('lazer', 'assets/Lazer.png');
     this.load.image('ship', 'assets/Ship.png');
@@ -43,8 +43,18 @@ function create() {
     var PADDING = 5;
     var style = { font: 'bold 15pt Arial', fill: 'white', align: 'left', wordWrap: { width: SCENE_WIDTH - PADDING * 2, useAdvancedWrap: false } };
 
+
+    var graphics = this.add.graphics(0, 0);
+    graphics.fillStyle(0);
+
+    graphics.fillRect(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
+    graphics.setInteractive();
+
     var text = this.add.text(0, 0, "", style);
     addText(text, fillText);
+
+    //graphics.on('gameobjectdown', changeState);
+    this.input.keyboard.on("keydown", changeState);
 }
 
 
@@ -52,13 +62,14 @@ function addText(textFill, fillText) {
     if (textFill.text != fillText) {
         var nextText = fillText.slice(0, textFill.text.length + 1);
         textFill.setText(nextText);
-        setTimeout(addText.bind(this, textFill, fillText), TEXT_TIMEOUT);
+        timerId = setTimeout(addText.bind(this, textFill, fillText), TEXT_TIMEOUT);
     } else {
-        setTimeout(changeState, END_TIMEOUT);
+        timerId = setTimeout(changeState, END_TIMEOUT);
     }
 }
 
 
 function changeState() {
+    clearTimeout(timerId)
     game.scene.switch("load", "play");
 }
